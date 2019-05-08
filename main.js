15ea1872edebaecde151e340cb5de6e040369422
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2019-05-08 11:48:51 -0700

    Break prod

--- main.js
+++ main.js
@@ -5,10 +5,14 @@ const port = 8080;
 
 console.log(process.env.ENVIRONMENT || 'e2e');
 
+if (process.env.ENVIRONMENT === 'prod') {
+    process.exit(1);
+}
+
 const server = http.createServer((_, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World.\n');
+  res.end('Hello, World. Updated!\n');
 });
 
 server.listen(port, hostname, () => {
