b687527271d7ba3823fd45ae426c551b47c54852
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2019-05-08 09:56:33 -0700

    Revert "Break prod"
    This reverts commit 44d732ed8510c1c2496c4b23b1c8b7c00c4a3ff5.

--- main.js
+++ main.js
@@ -3,16 +3,12 @@ const http = require('http');
 const hostname = '0.0.0.0';
 const port = 8080;
 
-if (process.env.ENVIRONMENT === 'prod') {
-    process.exit(1);
-}
-
 console.log(process.env.ENVIRONMENT || 'e2e');
 
 const server = http.createServer((_, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World. Version 2.\n');
+  res.end('Hello, World.\n');
 });
 
 server.listen(port, hostname, () => {
