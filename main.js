1286dceb3bbcce7fb5e32d11b379035931d6fd8d
Deepthi <deepthi.panthula@gmail.com>
2019-08-08 11:20:40 -0700

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ console.log(process.env.ENVIRONMENT || 'e2e');
 const server = http.createServer((_, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  res.end('Hello, World Test!\n');
 });
 
 server.listen(port, hostname, () => {
