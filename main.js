8169083baf6886f87ce552ff08f238a2a8dbbad0
Deepthi <deepthi.panthula@gmail.com>
2019-08-09 12:53:46 -0700

    Update main.js

--- main.js
+++ main.js
@@ -5,11 +5,16 @@ const port = 8080;
 
 console.log(process.env.ENVIRONMENT || 'e2e');
 
+if (process.env.ENVIRONMENT === 'prod') {	
+    process.exit(1);	
+}
+
 const server = http.createServer((_, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World Test!\n');
+  res.end('Hello, Jenkins World!\n');
 });
+
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
