1c07504ddc591c57aadd6ffdfb8c7abb6eeb4f60
Deepthi <deepthi.panthula@gmail.com>
2019-08-13 14:26:27 -0700

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ if (process.env.ENVIRONMENT === 'prod') {
 const server = http.createServer((_, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Jenkins World!\n');
+  res.end('Hello, Everyone!\n');
 });
 
 
